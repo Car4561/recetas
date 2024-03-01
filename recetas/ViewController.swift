3dc2a76165727e6bdadbb26547638a101b88ecb7
Carlos Llerena <cllerenh@emeal.nttdata.com>
2024-03-01 04:56:34 -0500

    Initial Commit

---
+++ recetas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  recetas
+//
+//  Created by Carlos Alfredo Llerena Huayta on 1/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
